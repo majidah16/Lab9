b3ed8320128ef7bb536b9cdb9ef5098c9c816e50
majidah16 <majidah.bidin16@gmail.com>
2022-06-23 22:43:44 +0800

    commit

--- next_page.php
+++ next_page.php
@@ -2,10 +2,13 @@
     session_start();
     echo "<html>";
 
-    $sid = session_id();
-    echo "Session ID returned by session_id(): ".$sid."\n";
-    $sid = SID;
-    echo "Session ID returned by SID: ".$sid."\n";
+    $queryString = $_SERVER ["QUERY STRING"];
+    echo "Query string of the incoming URL: ".$queryString."\n";
+
+    echo "Cookies received:\n";
+    foreach ($_COOKIE as $name => $value) {
+        echo " $name = $value\n";
+    }
 
     $myLogin = $_SESSION ["myLogin"];
     echo "Value of myLogin has been retrieved: ".$myLogin."\n";
