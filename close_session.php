3083b8fe7fa23416c5275f9651c24c368e674944
majidah16 <majidah.bidin16@gmail.com>
2022-06-23 22:53:31 +0800

    commit

---
+++ close_session.php
@@ -0,0 +1,17 @@
+<?php
+    session_start();
+
+    $_SESSION = array ();
+
+    if (isset($_COOKIE[session_name()])) {
+        setcookie(session_name(), '', time() -42000, '/');
+    }
+
+    session_destroy();
+
+    echo "<html>";
+    echo "Thank you for visiting FYICenter.com.<br>";
+    echo " <a href=login.php>Login Again.</a><br>";
+    echo "</html><br>";
+
+?>
