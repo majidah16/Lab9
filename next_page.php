c76e9dac1e468391176516e5d05329ae31bbe2f7
majidah16 <majidah.bidin16@gmail.com>
2022-06-23 22:23:24 +0800

    commit

---
+++ next_page.php
@@ -0,0 +1,17 @@
+<?php
+    session_start();
+    echo "<html>";
+
+    $sid = session_id();
+    echo "Session ID returned by session_id(): ".$sid."\n";
+    $sid = SID;
+    echo "Session ID returned by SID: ".$sid."\n";
+
+    $myLogin = $_SESSION ["myLogin"];
+    echo "Value of myLogin has been retrieved: ".$myLogin."\n";
+    $myColor = $_SESSION ["myColor"];
+    echo "Value of myColor has been retrieved: ".$myColor."\n";
+
+    echo "</html>\n";
+?>
+
